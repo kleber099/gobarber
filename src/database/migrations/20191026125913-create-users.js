d210382202cbf87d248d56c31c0e945f6c732365
kleber099 <kleber099@gmail.com>
2019-10-26 15:22:14 -0300

    Criando o loader de models

--- src/database/migrations/20191026125913-create-users.js
+++ src/database/migrations/20191026125913-create-users.js
@@ -3,9 +3,9 @@ module.exports = {
     return queryInterface.createTable('users', {
       id: {
         type: Sequelize.INTEGER,
+        primaryKey: true,
         allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
+        autoIncrement: true
       },
       name: {
         type: Sequelize.STRING,
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'kleber Mesquita',
+    email: 'kleber100@gmail.com',
+    password_hash: '12341234'
+  });
+
+  return res.json(user);
 });
 
 export default routes;
