e8953109f66d86f36ead3f6e7b8e0cb6a40e4620
kleber099 <kleber099@gmail.com>
2019-10-26 08:51:00 -0300

    configurando estrutura de pastas e sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'posgres',
+  password: 'docker',
+  database: 'gobarber',
+  difine: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
