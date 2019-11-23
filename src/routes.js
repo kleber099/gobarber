624a00b92cc0e63dcea0d7540e1c2f4e2455c981
kleber099 <kleber099@gmail.com>
2019-11-23 16:39:53 -0300

    Adicionando a taleba Files e relacionando com o usuario

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddelware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -17,8 +18,6 @@ routes.use(authMiddelware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
