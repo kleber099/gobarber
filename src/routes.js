f2384e713226130ba885f56a88a3a64273959a96
kleber099 <kleber099@gmail.com>
2019-11-24 13:26:48 -0300

    Adicionando listagem de provedores de serviço

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddelware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -17,6 +18,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddelware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
