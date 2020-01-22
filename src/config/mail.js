301ce8e0e5ecf591f558a8d91c49a6dac28bad4c
kleber099 <kleber099@gmail.com>
2020-01-21 21:24:36 -0300

    Configurando e enviando email com nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6c5d0bb27421aa',
+    pass: 'acc1bd54a09e71'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
